09cea4448b9f8af9c8c38044f636118816ab5b34
Алина Юзлибаева <91687337+euile@users.noreply.github.com>
2023-04-04 09:44:31 +0300

    Update main_Robot.cpp

--- main_Robot/main_Robot.cpp
+++ main_Robot/main_Robot.cpp
@@ -38,7 +38,7 @@ public:
 int main(int argc, char **argv) {
 
     //char mainWindow[] = "Main";
-	//char trackbarWindow_pink[] = "Trackbar_pink";
+	//char trackbarWindow_blue[] = "Trackbar_pink";
 	char thresholdWindow_pink[] = "Threshold_pink";
     char thresholdWindow_blue[] = "Threshold_blue";
 	int min = 0, max = 1000;
@@ -48,7 +48,7 @@ int main(int argc, char **argv) {
     int blue_hmin = 92, blue_smin = 221, blue_vmin = 156,
 		blue_hmax = 118, blue_smax = 255, blue_vmax = 207;
 
-	Mat frame, HSV, threshold, blurred;
+	Mat frame, HSV, threshold_pink, threshold_blue, blurred;
 
 
     // If the input is the web camera, pass 0 instead of the video file name
@@ -56,7 +56,7 @@ int main(int argc, char **argv) {
 
     //Создаем окна
 	//namedWindow(mainWindow, 0); хз зачем это окно
-	//namedWindow(trackbarWindow_pink, 0);
+	namedWindow(trackbarWindow_blue, 0);
 	namedWindow(thresholdWindow_pink, 0);
     namedWindow(thresholdWindow_blue, 0);
 
@@ -70,6 +70,16 @@ int main(int argc, char **argv) {
     createTrackbar("V max:", trackbarWindow_pink, &pink_vmax, pink_vmax);
     createTrackbar("Size min:", trackbarWindow_pink, &min, max);
     createTrackbar("Size max:", trackbarWindow_pink, &max, max);
+    */
+   /*
+    createTrackbar("H min:", trackbarWindow_blue, &blue_hmin, blue_hmax);
+    createTrackbar("H max:", trackbarWindow_blue, &blue_hmax, blue_hmax);
+    createTrackbar("S min:", trackbarWindow_blue, &blue_smin, blue_smax);
+    createTrackbar("S max:", trackbarWindow_blue, &blue_smax, blue_smax);
+    createTrackbar("V min:", trackbarWindow_blue, &blue_vmin, blue_vmax);
+    createTrackbar("V max:", trackbarWindow_blue, &blue_vmax, blue_vmax);
+    createTrackbar("Size min:", trackbarWindow_blue, &min, max);
+    createTrackbar("Size max:", trackbarWindow_blue, &max, max);
     */
 
     // Check if camera opened successfully
@@ -94,26 +104,45 @@ int main(int argc, char **argv) {
 
         int Xc_blue = 0;
         int Yc_blue = 0;
-        int counter_blue = 0; // счётчик числа белых пикселей (blue)
+        int counter_blue = 0; // счётчик числа белых пикселей (pink)
 
         cvtColor(frame, HSV, COLOR_BGR2HSV);
         medianBlur(HSV, blurred, 21);
-	    inRange(blurred, Scalar(pink_hmin, pink_smin, pink_vmin), Scalar(pink_hmax, pink_smax, pink_vmax), threshold);
-        inRange(blurred, Scalar(blue_hmin, blue_smin, blue_vmin), Scalar(blue_hmax, blue_smax, blue_vmax), threshold);
-        for(int y = 0; y < threshold.rows; y++){
-		    for(int x = 0; x < threshold.cols; x++){
-			    int value = threshold.at<uchar>(y, x);
+	    
+        inRange(blurred, Scalar(blue_hmin, blue_smin, blue_vmin), Scalar(blue_hmax, blue_smax, blue_vmax), threshold_blue);
+        for(int y = 0; y < threshold_blue.rows; y++){
+		    for(int x = 0; x < threshold_blue.cols; x++){
+			    int value = threshold_blue.at<uchar>(y, x);
 			    if(value == 255){
 				    Rect rect;
-				    int count = floodFill(threshold, Point(x, y), Scalar(200), &rect);
+				    int count = floodFill(threshold_blue, Point(x, y), Scalar(200), &rect);
+                    /* а че это они тут вместе считаются? м? почему координаты двух цветов считаются вместе? думайте!  */
+                    
+                    Xc_blue += x;
+                    Yc_blue += y;
+                    counter_blue++;
+				    if(rect.width >= min && rect.width <= max
+					    && rect.height >= min && rect.height <= max)
+                    {
+					    rectangle(frame, rect, Scalar(255, 0, 0, 4));
+                    }
+                }
+            }
+        }
+        
+        inRange(blurred, Scalar(pink_hmin, pink_smin, pink_vmin), Scalar(pink_hmax, pink_smax, pink_vmax), threshold_pink);
+
+        for(int y = 0; y < threshold_pink.rows; y++){
+		    for(int x = 0; x < threshold_pink.cols; x++){
+			    int value = threshold_pink.at<uchar>(y, x);
+			    if(value == 255){
+				    Rect rect;
+				    int count = floodFill(threshold_pink, Point(x, y), Scalar(200), &rect);
                     Xc_pink += x;
                     Yc_pink += y;
                     counter_pink++;
                     /* а че это они тут вместе считаются? м? почему координаты двух цветов считаются вместе? думайте!  */
 
-                    Xc_blue += x;
-                    Yc_blue += y;
-                    counter_blue++;
 				    if(rect.width >= min && rect.width <= max
 					    && rect.height >= min && rect.height <= max)
                     {
@@ -122,16 +151,22 @@ int main(int argc, char **argv) {
                 }
             }
         }
-        if (counter_pink != 0)
+        /*
+        cout << pink_hmin << ' ' << pink_hmax << ' ' << pink_smin <<' ' << pink_smax << ' ' << pink_vmin << ' ' << pink_vmax << endl;
+        cout << blue_hmin << ' ' << blue_hmax << ' ' << blue_smin << ' ' << blue_smax << ' ' << blue_vmin << ' ' << blue_vmax << endl;
+        */
+
+        if (counter_pink != 0 || counter_blue != 0)
         {
             cout << "Центр X: " << double(Xc_pink) / counter_pink  << ' ' << double(Xc_blue) / counter_blue << endl; 
             cout << "Центр Y: " << double(Yc_pink) / counter_pink << ' ' << double(Yc_blue) / counter_blue << endl;
         }
+        
 
         // Display the resulting frame
         imshow("Frame", frame);
-        imshow(thresholdWindow_pink, threshold);
-        imshow(thresholdWindow_blue, threshold);
+        imshow(thresholdWindow_pink, threshold_pink);
+        imshow(thresholdWindow_blue, threshold_blue);
         // Press  ESC on keyboard to exit
         char c = (char)waitKey(25);
         if (c == 27)
