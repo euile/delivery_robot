9895315e15a7c6c8598ec7f1abf8584aaecfaf2a
Алина Юзлибаева <91687337+euile@users.noreply.github.com>
2023-05-22 12:02:41 +0300

    Update main_Robot.cpp
    Теперь высчитываются все данные о перемещении робота!

--- main_Robot.cpp
+++ main_Robot.cpp
@@ -1,59 +1,31 @@
 #include "opencv2/opencv.hpp"
 #include <iostream>
 #include <math.h>
+#include <vector>
 
 using namespace std;
 using namespace cv;
 
-struct Color {
-    int R;
-    int G;
-    int B;
-};
-
-class Spot {
-private:
-    Color color;
-    Mat frame;
-    Mat spot;
-    pair <int, int> center;
-public:
-    void search_spot();
-    void count_center();
-};
-
-class Route {
-private:
-    Spot back;
-    Spot front;
-    int rotation;
-    int distance;
-
-public:
-    int count_rotation(pair <int, int> destination);
-    int count_distance(pair <int, int> start, pair <int, int> stop);
-};
-
-
+// Вывод в формате all_data = [["угол до старта", "dist до старта"], ["угол до доз", "dist до доз"], ["угол до пуф", "dist до пуф"]]
 
 int main(int argc, char **argv) {
 
     //char mainWindow[] = "Main";
-    //char trackbarWindow_blue[] = "Trackbar_pink";
-    char thresholdWindow_pink[] = "Threshold_pink";
+	//char trackbarWindow_blue[] = "Trackbar_pink";
+	char thresholdWindow_pink[] = "Threshold_pink";
     char thresholdWindow_blue[] = "Threshold_blue";
     char thresholdWindow_yellow[] = "Threshold_yellow";
-    int min = 0, max = 1000;
-    int pink_hmin = 157, pink_smin = 119, pink_vmin = 187,
-	pink_hmax = 186, pink_smax = 200, pink_vmax = 255;
+	int min = 0, max = 1000;
+	int pink_hmin = 157, pink_smin = 119, pink_vmin = 187,
+		pink_hmax = 186, pink_smax = 200, pink_vmax = 255;
 
     int blue_hmin = 92, blue_smin = 221, blue_vmin = 156,
-	blue_hmax = 118, blue_smax = 255, blue_vmax = 207;
+		blue_hmax = 118, blue_smax = 255, blue_vmax = 207;
 
     int yellow_hmin = 43, yellow_smin = 47, yellow_vmin = 170,
-	yellow_hmax = 87, yellow_smax = 87, yellow_vmax = 255;
+		yellow_hmax = 87, yellow_smax = 87, yellow_vmax = 255;
 
-    Mat frame, HSV, threshold_pink, threshold_blue, threshold_yellow, blurred;
+	Mat frame, HSV, threshold_pink, threshold_blue, threshold_yellow, blurred;
 
 
     // If the input is the web camera, pass 0 instead of the video file name
@@ -62,7 +34,7 @@ int main(int argc, char **argv) {
     //Создаем окна
 	//namedWindow(mainWindow, 0); хз зачем это окно
 	//namedWindow(trackbarWindow_blue, 0);
-    namedWindow(thresholdWindow_pink, 0);
+	namedWindow(thresholdWindow_pink, 0);
     namedWindow(thresholdWindow_blue, 0);
     namedWindow(thresholdWindow_yellow, 0);
 
@@ -92,69 +64,71 @@ int main(int argc, char **argv) {
         int Yc_yellow = 0;
         int counter_yellow = 0; // счётчик числа белых пикселей (yellow)
 
+        int X_dispenser = 100; // центр дозатора
+        int Y_dispenser = 100;
+
+        int X_start = 100;  // центр точки старта
+        int Y_start = 300;
+
         cvtColor(frame, HSV, COLOR_BGR2HSV);
         medianBlur(HSV, blurred, 21);
-	    
+        
         inRange(blurred, Scalar(blue_hmin, blue_smin, blue_vmin), Scalar(blue_hmax, blue_smax, blue_vmax), threshold_blue);
-        for(int y = 0; y < threshold_blue.rows; y++)
-	{
-	    for(int x = 0; x < threshold_blue.cols; x++)
-	    {
-		int value = threshold_blue.at<uchar>(y, x);
-		if (value == 255)
-		{
-		    Rect rect;
-		    int count = floodFill(threshold_blue, Point(x, y), Scalar(200), &rect);
+        for(int y = 0; y < threshold_blue.rows; y++){
+		    for(int x = 0; x < threshold_blue.cols; x++){
+			    int value = threshold_blue.at<uchar>(y, x);
+			    if(value == 255){
+				    Rect rect;
+				    int count = floodFill(threshold_blue, Point(x, y), Scalar(200), &rect);
+                    
                     Xc_blue += x;
                     Yc_blue += y;
                     counter_blue++;
-		    if (rect.width >= min && rect.width <= max && rect.height >= min && rect.height <= max)
+				    if(rect.width >= min && rect.width <= max
+					    && rect.height >= min && rect.height <= max)
                     {
-			rectangle(frame, rect, Scalar(255, 0, 0, 4));
+					    rectangle(frame, rect, Scalar(255, 0, 0, 4));
                     }
                 }
             }
         }
         
         inRange(blurred, Scalar(pink_hmin, pink_smin, pink_vmin), Scalar(pink_hmax, pink_smax, pink_vmax), threshold_pink);
-        for(int y = 0; y < threshold_pink.rows; y++)
-	{
-	    for(int x = 0; x < threshold_pink.cols; x++)
-	    {
-	        int value = threshold_pink.at<uchar>(y, x);
-		if (value == 255)
-		{
-		    Rect rect;
-		    int count = floodFill(threshold_pink, Point(x, y), Scalar(200), &rect);
+
+        for(int y = 0; y < threshold_pink.rows; y++){
+		    for(int x = 0; x < threshold_pink.cols; x++){
+			    int value = threshold_pink.at<uchar>(y, x);
+			    if(value == 255){
+				    Rect rect;
+				    int count = floodFill(threshold_pink, Point(x, y), Scalar(200), &rect);
                     Xc_pink += x;
                     Yc_pink += y;
                     counter_pink++;
                     
-		    if (rect.width >= min && rect.width <= max && rect.height >= min && rect.height <= max)
+				    if(rect.width >= min && rect.width <= max
+					    && rect.height >= min && rect.height <= max)
                     {
-			rectangle(frame, rect, Scalar(255, 0, 255, 4));
+					    rectangle(frame, rect, Scalar(255, 0, 255, 4));
                     }
                 }
             }
         }
 
         inRange(blurred, Scalar(yellow_hmin, yellow_smin, yellow_vmin), Scalar(yellow_hmax, yellow_smax, yellow_vmax), threshold_yellow);
-        for(int y = 0; y < threshold_yellow.rows; y++)
-	{
-	    for(int x = 0; x < threshold_yellow.cols; x++)
-	    {
-	        int value = threshold_yellow.at<uchar>(y, x);
-		if (value == 255)
-		{
-		    Rect rect;
-		    int count = floodFill(threshold_yellow, Point(x, y), Scalar(200), &rect);
+        for(int y = 0; y < threshold_yellow.rows; y++){
+		    for(int x = 0; x < threshold_yellow.cols; x++){
+			    int value = threshold_yellow.at<uchar>(y, x);
+			    if(value == 255){
+				    Rect rect;
+				    int count = floodFill(threshold_yellow, Point(x, y), Scalar(200), &rect);
                     
                     Xc_yellow += x;
                     Yc_yellow += y;
                     counter_yellow++;
-		    if (rect.width >= min && rect.width <= max && rect.height >= min && rect.height <= max)
+				    if(rect.width >= min && rect.width <= max
+					    && rect.height >= min && rect.height <= max)
                     {
-			rectangle(frame, rect, Scalar(255, 255, 0, 4));
+					    rectangle(frame, rect, Scalar(255, 255, 0, 4));
                     }
                 }
             }
@@ -164,24 +138,55 @@ int main(int argc, char **argv) {
         cout << pink_hmin << ' ' << pink_hmax << ' ' << pink_smin <<' ' << pink_smax << ' ' << pink_vmin << ' ' << pink_vmax << endl;
         cout << blue_hmin << ' ' << blue_hmax << ' ' << blue_smin << ' ' << blue_smax << ' ' << blue_vmin << ' ' << blue_vmax << endl;
         */
-
+        vector <vector<double>> all_data;
         if (counter_pink != 0 && counter_blue != 0 && counter_yellow != 0)
         {
             double x1 = double(Xc_pink) / counter_pink - double(Xc_blue) / counter_blue; // Px - Bx
-            double x2 = double(Xc_yellow) / counter_yellow - double(Xc_blue) / counter_blue; // Yx - Bx
             double y1 = double(Yc_pink) / counter_pink - double(Yc_blue) / counter_blue; // Py - By
+            double x2 = double(Xc_yellow) / counter_yellow - double(Xc_blue) / counter_blue; // Yx - Bx
             double y2 = double(Yc_yellow) / counter_yellow - double(Yc_blue) / counter_blue; // Yy - By
+            double x3 = double(X_dispenser) - double(Xc_blue) / counter_blue; // Dx - Bx
+            double y3 = double(Y_dispenser) - double(Yc_blue) / counter_blue; // Dy - By
+            double x4 = double(X_start) - double(Xc_blue) / counter_blue; // Sx - Bx
+            double y4 = double(Y_start) - double(Yc_blue) / counter_blue; // Sy - By
+
 
             double BP = sqrt(x1 * x1 + y1 * y1);
             double BY = sqrt(x2 * x2 + y2 * y2);
-            double argument = (x1 * x2 + y1 * y2) / (BY * BP);
+            double BD = sqrt(x3 * x3 + y3 * y3);
+            double BS = sqrt(x4 * x4 + y4 * y4);
+            double cos_to_puf = (x1 * x2 + y1 * y2) / (BY * BP);
+            double cos_to_disp = (x1 * x3 + y1 * y3) / (BP * BD);
+            double cos_to_start = (x1 * x4 + y1 * y4) / (BP * BS);
 
-            double distance = sqrt(pow(double(Xc_yellow) / counter_yellow - double(Xc_pink) / counter_pink, 2) +\
+
+            double dist_to_puf = sqrt(pow(double(Xc_yellow) / counter_yellow - double(Xc_pink) / counter_pink, 2) +\
             pow(double(Yc_yellow) / counter_yellow - double(Yc_pink) / counter_pink, 2));
-            cout << "Угол поворота: " << acos(argument) << endl;
-            cout << "Расстояние до пуфика: " << distance << endl;
-            //cout << "Центр X: " << double(Xc_pink) / counter_pink  << ' ' << double(Xc_blue) / counter_blue << ' ' << double(Xc_red) / counter_red << endl; 
-            //cout << "Центр Y: " << double(Yc_pink) / counter_pink << ' ' << double(Yc_blue) / counter_blue  << ' ' << double(Yc_red) / counter_red << endl;
+            double dist_to_disp = sqrt(pow(double(X_dispenser)- double(Xc_pink) / counter_pink, 2) +\
+            pow(double(Y_dispenser) - double(Yc_pink) / counter_pink, 2));
+            double dist_to_start = sqrt(pow(double(X_start)- double(Xc_pink) / counter_pink, 2) +\
+            pow(double(Y_start) - double(Yc_pink) / counter_pink, 2));
+            
+            for (int i = 0; i < 3; ++i)
+            {
+                if (i == 0)
+                {
+                    vector <double> temp = {acos(cos_to_start), dist_to_start};
+                    cout << acos(cos_to_start) << "\t" << dist_to_start;
+                    all_data.push_back(temp);
+                } else if (i == 1)
+                {
+                    vector <double> temp = {acos(cos_to_disp), dist_to_disp};
+                    cout << acos(cos_to_disp) << "\t" << dist_to_disp;
+                    all_data.push_back(temp);
+                } else 
+                {
+                    vector <double> temp = {acos(cos_to_puf), dist_to_puf};
+                    cout << acos(cos_to_puf) << "\t" << dist_to_puf;
+                    all_data.push_back(temp);
+                }
+            }
+            cout << endl;
         }
         
         // Display the resulting frame
