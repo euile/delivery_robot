ce3416351005a7b1a36e6247321d1ae62b62e7f9
Sofia-Oleynik <113835759+Sofia-Oleynik@users.noreply.github.com>
2023-05-22 22:53:50 +0300

    Update main_Robot.h

--- main_Robot.h
+++ main_Robot.h
@@ -1,6 +1,10 @@
 #pragma once
 #include <vector>
+#include <iostream>
+#include <math.h>
+#include "opencv2/opencv.hpp"
+
+using namespace std;
+using namespace cv;
 
 vector<vector<double>> coords();
-
-
